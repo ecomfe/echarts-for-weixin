5608e304dcaa90a191a363c793d1acde73b8b1e7
Ovilia <zwl.sjtu@gmail.com>
2018-05-28 16:29:24 +0800

    test: update line test case

--- pages/line/index.js
+++ pages/line/index.js
@@ -10,15 +10,17 @@ function initChart(canvas, width, height) {
   canvas.setChart(chart);
 
   var option = {
-    backgroundColor: "#fff",
-    color: ["#37A2DA", "#67E0E3", "#9FE6B8"],
-
-    tooltip: {
-      trigger: 'axis'
+    title: {
+      text: '测试下面legend的红色区域不应被裁剪',
+      left: 'center'
     },
+    color: ["#37A2DA", "#67E0E3", "#9FE6B8"],
     legend: {
-
-      data: ['A商品', 'B商品', 'C商品']
+      data: ['A', 'B', 'C'],
+      top: 50,
+      left: 'center',
+      backgroundColor: 'red',
+      z: 100
     },
     grid: {
       containLabel: true
@@ -27,28 +29,35 @@ function initChart(canvas, width, height) {
     xAxis: {
       type: 'category',
       boundaryGap: false,
-      data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+      data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+      // show: false
     },
     yAxis: {
       x: 'center',
-      type: 'value'
+      type: 'value',
+      splitLine: {
+        lineStyle: {
+          type: 'dashed'
+        }
+      }
+      // show: false
     },
     series: [{
-      name: 'A商品',
+      name: 'A',
       type: 'line',
       smooth: true,
       data: [18, 36, 65, 30, 78, 40, 33]
     }, {
-      name: 'B商品',
+      name: 'B',
       type: 'line',
       smooth: true,
       data: [12, 50, 51, 35, 70, 30, 20]
     }, {
-      name: 'C商品',
+      name: 'C',
       type: 'line',
       smooth: true,
       data: [10, 30, 31, 50, 40, 20, 10]
-    }]
+}]
   };
 
   chart.setOption(option);
