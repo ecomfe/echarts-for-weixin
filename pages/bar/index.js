0dc215c849e63459e5e3bc1b962ecdb10008aafb
Ovilia <zwl.sjtu@gmail.com>
2018-11-02 15:03:30 +0800

    chore: update echarts version

--- pages/bar/index.js
+++ pages/bar/index.js
@@ -15,7 +15,8 @@ function initChart(canvas, width, height) {
       trigger: 'axis',
       axisPointer: {            // 坐标轴指示器，坐标轴触发有效
         type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-      }
+      },
+      confine: true
     },
     legend: {
       data: ['热度', '正面', '负面']
