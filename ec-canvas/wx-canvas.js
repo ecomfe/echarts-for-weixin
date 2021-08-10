306f729e306f5f969b5e2994316f91528589dc71
Ovilia <zwl.sjtu@gmail.com>
2021-08-10 13:07:58 +0800

    remove defineProperty

--- ec-canvas/wx-canvas.js
+++ ec-canvas/wx-canvas.js
@@ -53,22 +53,6 @@ export default class WxCanvas {
   }
 
   _initStyle(ctx) {
-    var styles = ['fillStyle', 'strokeStyle', 'globalAlpha',
-      'textAlign', 'textBaseAlign', 'shadow', 'lineWidth',
-      'lineCap', 'lineJoin', 'lineDash', 'miterLimit', 'fontSize'];
-
-    styles.forEach(style => {
-      Object.defineProperty(ctx, style, {
-        set: value => {
-          if (style !== 'fillStyle' && style !== 'strokeStyle'
-            || value !== 'none' && value !== null
-          ) {
-            ctx['set' + style.charAt(0).toUpperCase() + style.slice(1)](value);
-          }
-        }
-      });
-    });
-
     ctx.createRadialGradient = () => {
       return ctx.createCircularGradient(arguments);
     };
