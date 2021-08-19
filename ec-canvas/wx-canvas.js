4b4751a5ad1a7cae43e21526fe3dd8c6204965c1
huan.yang <huan.yang@yunzhanghu.com>
2021-08-19 15:07:30 +0800

    fix: revert canvas _initStyle by qywx

--- ec-canvas/wx-canvas.js
+++ ec-canvas/wx-canvas.js
@@ -53,6 +53,22 @@ export default class WxCanvas {
   }
 
   _initStyle(ctx) {
+    var styles = ['fillStyle', 'strokeStyle', 'globalAlpha', 
+      'textAlign', 'textBaseAlign', 'shadow', 'lineWidth',
+      'lineCap', 'lineJoin', 'lineDash', 'miterLimit', 'fontSize'];
+
+    styles.forEach(style => {
+      Object.defineProperty(ctx, style, {
+        set: value => {
+          if (style !== 'fillStyle' && style !== 'strokeStyle' 
+            || value !== 'none' && value !== null
+          ) {
+            ctx['set' + style.charAt(0).toUpperCase() + style.slice(1)](value);
+          }
+        }
+      });
+    });
+    
     ctx.createRadialGradient = () => {
       return ctx.createCircularGradient(arguments);
     };
